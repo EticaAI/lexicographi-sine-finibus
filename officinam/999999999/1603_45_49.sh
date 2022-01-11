54f216b84c00e2bfd33915cf9a3df212ce9ba171
Emerson Rocha <rocha@ieee.org>
2022-01-11 04:31:14 -0300

    1603:13 #5: 1603_45_49.sh code refactoring

--- officinam/999999999/1603_45_49.sh
+++ officinam/999999999/1603_45_49.sh
@@ -30,7 +30,7 @@ set -e
 
 ROOTDIR="$(pwd)"
 
-PRAEFIXUM="1603_45_49:"
+PRAEFIXUM="1603:45:49:"
 
 # Source:
 # - https://vocabulary.unocha.org/
@@ -39,67 +39,160 @@ PRAEFIXUM="1603_45_49:"
 
 DATA_UN_M49_CSV="https://proxy.hxlstandard.org/data.csv?dest=data_edit&filter01=cut&cut-skip-untagged01=on&filter02=sort&sort-tags02=%23country%2Bcode%2Bnum%2Bv_m49&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY%2Fedit%23gid%3D1088874596"
 
-# TODO: implement option to rebuild even if file already on disk
-if [ ! -f "${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv" ]; then
-  wget -qO- "$DATA_UN_M49_CSV" >"${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv"
-else
-  echo "Cached: ${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv"
-fi
+# shellcheck source=999999999.lib.sh
+. "$ROOTDIR"/999999999/999999999.lib.sh
+
+#######################################
+# Download 1603_45_49 from external source files
+#
+# Globals:
+#   ROOTDIR
+#   DATA_UN_M49_CSV
+# Arguments:
+#   None
+# Outputs:
+#   Writes to 999999/1603/45/49/1603_45_49.hxl.csv
+#######################################
+1603_45_49__external_fetch() {
+  objectivum_archivum="${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv"
+  objectivum_archivum_temporarium="${ROOTDIR}/999999/0/1603_45_49.hxl.csv"
+
+  if [ -z "$(stale_archive "$objectivum_archivum")" ]; then return 0; fi
+
+  echo "${FUNCNAME[0]} stale data on [$objectivum_archivum], refreshing..."
+
+  curl --header "Accept: text/csv" \
+    --compressed --silent --show-error \
+    --get "$DATA_UN_M49_CSV" \
+    --output "$objectivum_archivum_temporarium"
+
+  file_update_if_necessary csv "$objectivum_archivum_temporarium" "$objectivum_archivum"
+}
 
 ### 1603_45_49.hxl.csv --> 1603_45_49.tm.hxl.csv _______________________________
+#######################################
+# Download external source files.
+# Note: using raw tab file for now.
+#
+# Globals:
+#   ROOTDIR
+# Arguments:
+#   [File] 999999/1603/45/49/1603_45_49.hxl.csv
+# Outputs:
+#   [File] 999999/1603/45/49/1603_45_49.tm.hxl.csv
+#######################################
+1603_45_49__hxl2hxltm() {
+  fontem_archivum="${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv"
+  objectivum_archivum="${ROOTDIR}/999999/1603/45/49/1603_45_49.tm.hxl.csv"
+  objectivum_archivum_temporarium="${ROOTDIR}/999999/0/1603_45_49.tm.hxl.csv"
 
-# Note: ix_iso3166p1 would generate -x-iso3166p1, 9 characters, but BCP limit to 8
-# Note: ix_unreliefweb would generate -x-unreliefweb, 11 characters, but BCP limit to 8
-hxlrename \
-  --rename="#country+name+i_en+alt+v_unterm:#item+rem+i_eng+is_latn+ix_unterm" \
-  --rename="#country+name+i_fr+alt+v_unterm:#item+rem+i_fra+is_latn+ix_unterm" \
-  --rename="#country+name+i_es+alt+v_unterm:#item+rem+i_spa+is_latn+ix_unterm" \
-  --rename="#country+name+i_ru+alt+v_unterm:#item+rem+i_rus+is_cyrl+ix_unterm" \
-  --rename="#country+name+i_zh+alt+v_unterm:#item+rem+i_zho+is_hans+ix_unterm" \
-  --rename="#country+name+i_ar+alt+v_unterm:#item+rem+i_ara+is_arab+ix_unterm" \
-  "${ROOTDIR}/999999/1603/45/49/1603_45_49.hxl.csv" |
-  hxlselect --query="#country+code+num+v_m49>0" |
-  hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unfts={{#country+code+v_fts}}" |
-  hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unreliefweb={{#country+code+v_reliefweb}}" |
-  hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unhrinfo={{#country+code+v_hrinfo_country}}" |
-  hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unm49={{#country+code+num+v_m49}}" |
-  hxladd --before --spec="#item+conceptum+codicem={{#country+code+num+v_m49}}" |
-  hxlsort --tags="#item+conceptum" \
-    >"${ROOTDIR}/999999/1603/45/49/1603_45_49.tm.hxl.csv"
+  if [ -z "$(changed_recently "$fontem_archivum")" ]; then return 0; fi
 
-# @TODO: only do this if hxl did not removed empty header files ,,,,,,
-sed -i '1d' "${ROOTDIR}/999999/1603/45/49/1603_45_49.tm.hxl.csv"
+  echo "${FUNCNAME[0]} sources changed_recently. Reloading..."
 
-### 1603_45_49.tm.hxl.csv --> 1603_45_49.no1.tm.hxl.csv ________________________
+  # Note: ix_iso3166p1 would generate -x-iso3166p1, 9 characters, but BCP limit to 8
+  # Note: ix_unreliefweb would generate -x-unreliefweb, 11 characters, but BCP limit to 8
 
-hxlrename \
-  --rename="#country+code+v_iso2:#item+rem+i_zxx+is_latn+ix_iso3166p1a2" \
-  --rename="#country+code+v_iso3:#item+rem+i_zxx+is_latn+ix_iso3166p1a3" \
-  "${ROOTDIR}/999999/1603/45/49/1603_45_49.tm.hxl.csv" |
-  hxladd --before --spec="#item+conceptum+numerordinatio=${PRAEFIXUM}{{(#item+conceptum+codicem)+1-1}}" |
-  hxlcut --include="#item+conceptum,#item+rem" \
-    >"${ROOTDIR}/1603/45/49/1603_45_49.no1.tm.hxl.csv"
+  hxlrename \
+    --rename="#country+name+i_en+alt+v_unterm:#item+rem+i_eng+is_latn+ix_unterm" \
+    --rename="#country+name+i_fr+alt+v_unterm:#item+rem+i_fra+is_latn+ix_unterm" \
+    --rename="#country+name+i_es+alt+v_unterm:#item+rem+i_spa+is_latn+ix_unterm" \
+    --rename="#country+name+i_ru+alt+v_unterm:#item+rem+i_rus+is_cyrl+ix_unterm" \
+    --rename="#country+name+i_zh+alt+v_unterm:#item+rem+i_zho+is_hans+ix_unterm" \
+    --rename="#country+name+i_ar+alt+v_unterm:#item+rem+i_ara+is_arab+ix_unterm" \
+    "${fontem_archivum}" |
+    hxlselect --query="#country+code+num+v_m49>0" |
+    hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unfts={{#country+code+v_fts}}" |
+    hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unreliefweb={{#country+code+v_reliefweb}}" |
+    hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unhrinfo={{#country+code+v_hrinfo_country}}" |
+    hxladd --before --spec="#item+rem+i_zxx+is_zmth+ix_unm49={{#country+code+num+v_m49}}" |
+    hxladd --before --spec="#item+conceptum+codicem={{#country+code+num+v_m49}}" |
+    hxlsort --tags="#item+conceptum" \
+      >"${objectivum_archivum_temporarium}"
 
-# @TODO: only do this if hxl did not removed empty header files ,,,,,,
-sed -i '1d' "${ROOTDIR}/1603/45/49/1603_45_49.no1.tm.hxl.csv"
+  # Strip empty header (already is likely to be ,,,,,,)
+  sed -i '1d' "${objectivum_archivum_temporarium}"
 
-hxladd \
-  --before --spec="#x_item+lower={{#item+rem+i_zxx+is_latn+ix_iso3166p1a2}}" \
-  --before --spec="#x_item+upper={{#item+rem+i_zxx+is_latn+ix_iso3166p1a2}}" \
-  "${ROOTDIR}/1603/45/49/1603_45_49.no1.tm.hxl.csv" |
-  hxladd --before --spec="#x_item+lower={{#item+rem+i_zxx+is_latn+ix_iso3166p1a3}}" |
-  hxladd --before --spec="#x_item+upper={{#item+rem+i_zxx+is_latn+ix_iso3166p1a3}}" |
-  hxladd --before --spec="#x_item={{#item+conceptum+codicem}}" |
-  hxladd --before --spec="#x_item={{#item+conceptum+codicem}}" |
-  hxlclean --lower="#x_item+lower" |
-  hxlclean --upper="#x_item+upper" |
-  hxlcut --include="#x_item" |
-  csvformat --out-tabs --skip-lines 2 |
-  sed 's/None//' | sed 's/None//' | sed 's/None//' | sed 's/None//' |
-  sed 's/NONE//' | sed 's/NONE//' | sed 's/NONE//' | sed 's/NONE//' |
-  sed 's/none//' | sed 's/none//' | sed 's/none//' | sed 's/none//' \
-  >"${ROOTDIR}/999999/999999/1603_45_49.tsv"
+  file_update_if_necessary csv "$objectivum_archivum_temporarium" "$objectivum_archivum"
+}
 
-# TODO: fix the "None from 1603.45.16.tsv"
+#######################################
+# Download external source files.
+# Note: using raw tab file for now.
+#
+# Globals:
+#   ROOTDIR
+# Arguments:
+#   [File] 999999/1603/45/49/1603_45_49.tm.hxl.csv
+# Outputs:
+#   [File] 1603/45/49/1603_45_49.no1.tm.hxl.csv
+#######################################
+1603_45_49__hxltm2numerordinatio() {
+  fontem_archivum="${ROOTDIR}/999999/1603/45/49/1603_45_49.tm.hxl.csv"
+  objectivum_archivum="${ROOTDIR}/1603/45/49/1603_45_49.no1.tm.hxl.csv"
+  objectivum_archivum_temporarium="${ROOTDIR}/999999/0/1603_45_49.no1.tm.hxl.csv"
+
+  if [ -z "$(changed_recently "$fontem_archivum")" ]; then return 0; fi
+
+  echo "${FUNCNAME[0]} sources changed_recently. Reloading..."
+
+  hxlrename \
+    --rename="#country+code+v_iso2:#item+rem+i_zxx+is_latn+ix_iso3166p1a2" \
+    --rename="#country+code+v_iso3:#item+rem+i_zxx+is_latn+ix_iso3166p1a3" \
+    "${fontem_archivum}" |
+    hxladd --before --spec="#item+conceptum+numerordinatio=${PRAEFIXUM}{{(#item+conceptum+codicem)+1-1}}" |
+    hxlcut --include="#item+conceptum,#item+rem" \
+      >"${objectivum_archivum_temporarium}"
+
+  # @TODO: only do this if hxl did not removed empty header files ,,,,,,
+  sed -i '1d' "${objectivum_archivum_temporarium}"
+
+  file_update_if_necessary csv "$objectivum_archivum_temporarium" "$objectivum_archivum"
+}
+
+#######################################
+# numerordinatio2tsv
+#
+# Globals:
+#   ROOTDIR
+#   DATA_UN_M49_CSV
+# Arguments:
+#   [File] 999999/1603/45/49/1603_45_49.tm.hxl.csv
+# Outputs:
+#   [File] 1603/45/49/1603_45_49.no1.tm.hxl.csv
+#######################################
+1603_45_49__numerordinatio2tsv() {
+  fontem_archivum="${ROOTDIR}/1603/45/49/1603_45_49.no1.tm.hxl.csv"
+  objectivum_archivum="${ROOTDIR}/999999/999999/1603_45_49.tsv"
+  objectivum_archivum_temporarium="${ROOTDIR}/999999/0/1603_45_49.tsv"
+
+  if [ -z "$(changed_recently "$fontem_archivum")" ]; then return 0; fi
+
+  echo "${FUNCNAME[0]} sources changed_recently. Reloading..."
+
+  hxladd \
+    --before --spec="#x_item+lower={{#item+rem+i_zxx+is_latn+ix_iso3166p1a2}}" \
+    --before --spec="#x_item+upper={{#item+rem+i_zxx+is_latn+ix_iso3166p1a2}}" \
+    "${fontem_archivum}" |
+    hxladd --before --spec="#x_item+lower={{#item+rem+i_zxx+is_latn+ix_iso3166p1a3}}" |
+    hxladd --before --spec="#x_item+upper={{#item+rem+i_zxx+is_latn+ix_iso3166p1a3}}" |
+    hxladd --before --spec="#x_item={{#item+conceptum+codicem}}" |
+    hxladd --before --spec="#x_item={{#item+conceptum+codicem}}" |
+    hxlclean --lower="#x_item+lower" |
+    hxlclean --upper="#x_item+upper" |
+    hxlcut --include="#x_item" |
+    csvformat --out-tabs --skip-lines 2 |
+    sed 's/None//' | sed 's/None//' | sed 's/None//' | sed 's/None//' |
+    sed 's/NONE//' | sed 's/NONE//' | sed 's/NONE//' | sed 's/NONE//' |
+    sed 's/none//' | sed 's/none//' | sed 's/none//' | sed 's/none//' \
+    >"${objectivum_archivum_temporarium}"
+
+  file_update_if_necessary csv "$objectivum_archivum_temporarium" "$objectivum_archivum"
+}
+
+1603_45_49__external_fetch
+1603_45_49__hxl2hxltm
+1603_45_49__hxltm2numerordinatio
+1603_45_49__numerordinatio2tsv
 
 set +x
--- officinam/999999999/999999999.lib.sh
+++ officinam/999999999/999999999.lib.sh
@@ -65,6 +65,10 @@ changed_recently() {
 #
 # Opposite: changed_recently
 #
+# About changed times with git clone, see alternatives:
+#  https://stackoverflow.com/a/21735986/894546
+#  https://stackoverflow.com/a/13284229/894546
+#
 # Globals:
 #   None
 # Arguments:
@@ -117,19 +121,19 @@ file_update_if_necessary() {
     ;;
   esac
 
-  echo "middle file_update_if_necessary ..."
+  # echo "middle file_update_if_necessary ..."
 
   if [ -f "$objectivum_archivum" ]; then
     sha256sum "$objectivum_archivum"
     sha256sum "$fontem_archivum"
-    # TODO: review this logic
-    if test "$(cmp --silent "$fontem_archivum" "$objectivum_archivum")"; then
-      echo "INFO: already equal."
+
+    if [ -s "$objectivum_archivum" ] && [ "$(cmp "$fontem_archivum" "$objectivum_archivum")" = "" ]; then
+      echo "INFO: already equal. Temporary will be discarted"
       echo "      [$fontem_archivum]"
       echo "      [$objectivum_archivum]"
       rm "$fontem_archivum"
     else
-      echo "not equal"
+      echo "Not equal. Temporary will replace target file"
       rm "$objectivum_archivum"
       mv "$fontem_archivum" "$objectivum_archivum"
     fi
